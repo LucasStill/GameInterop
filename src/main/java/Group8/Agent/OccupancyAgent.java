c08e32ec763b7a93ae53b45de2163d24cec8198b
Th0github <tdtsijpkens@gmail.com>
2020-05-02 16:58:16 +0200

    Added the variables for OccupancyAgent and instantiation. Next thing to do is figure out how pre-allocate all  grid positions into a tensor.

--- src/main/java/Group8/Agent/OccupancyAgent.java
+++ src/main/java/Group8/Agent/OccupancyAgent.java
@@ -8,14 +8,53 @@ import Interop.Percept.Sound.SoundPercepts;
 import Interop.Percept.Vision.FieldOfView;
 import Interop.Percept.Vision.VisionPrecepts;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Thomas Sijpkens
  * class OccupancyAgent uses and Occupancy grid representation of the environment.  This allows us to use ML approaches for the agent.
  */
 public class OccupancyAgent {
+    private double xsize; //X size of map
+    private double ysize; //Y size of map
+    private double grid_size;
+    private ArrayList<Double> log_prob_map; //initially set to zero
+
+    private double alpha = 1.0; //How thick can obstacles be
+    private double beta = 5.0 * Math.PI/180; //width of FOV.
+    //TODO: modify beta on how wide the FieldOfView is.
+    private double z_max = 150.0; //how much info could be stored from the laser
+
+    //Pre-allocate the x and y position of all the grid position on a 2D (might be 3D from the literature) tensor.  (Pre-process = faster)
+    //TODO: copy x and y position into grid_position_m
+    private ArrayList<Double> grid_position_m;
+
+    //Log-probabilities to add or remove from the map
+    private double log_occ = Math.log(0.65/0.35);
+    private double log_free = Math.log(0.35/0.65);
+
+    public OccupancyAgent(double xsize, double ysize, double grid_size) {
+        this.xsize = xsize;
+        this.ysize = ysize;
+        this.grid_size = grid_size;
+
+        //pre-allocate grid_position_m
+        //self.grid_position_m = np.array([np.tile(np.arange(0, self.xsize*self.grid_size, self.grid_size)[:,None], (1, self.ysize)),np.tile(np.arange(0, self.ysize*self.grid_size, self.grid_size)[:,None].T, (self.xsize, 1))])
+    }
+
+    public void updateMap(double[] pose, double[] z) {
+        List<Double> dx = new ArrayList<>(grid_position_m); //tensor of coordinates of all cells
+        //matrix of all x coordinate of cell
+        //matrix of all y coordinate of cell
+        //matrix of all bearings from robot to cell
+
+        //wrap to +pi/-pi
+
+        //for each laser beam(where we update
+    }
 
     //TODO: check if this is how perception works
-    //Line 19-42
     /**
      * @param visionPrecept the vision perception of a given agent
      * @return return a field of vision of a agent
